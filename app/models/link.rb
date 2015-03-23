59bcf0217c3ab214f63363eae77388046864849b
Mohnish G J <mail4mohnish@gmail.com>
2015-03-23 21:38:14 +0530

    regex to check for valid uri with http or https

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,11 +3,19 @@ class Link < ActiveRecord::Base
 
   validates :shortened_url, uniqueness: true
 
+  before_save :ensure_valid_uri
+
   after_create :generate_shortened_url
 
   private
+
+    def ensure_valid_uri
+      self.url.scan(URI.regexp).present?
+    end
+
     def generate_shortened_url
       self.shortened_url = SecureRandom.urlsafe_base64(6).gsub(/-|_/, (97+rand(27)).chr)
       self.save
     end
+
 end
