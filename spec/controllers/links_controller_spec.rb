e0286798cdfe6934f13738c20c475c9201652b7e
Mohnish G J <mail4mohnish@gmail.com>
2015-03-24 04:34:49 +0530

    specs for creating a link via the web app

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
+  describe '#create' do
+    context "given a valid uri" do
+      it "successfully creates the link record and saves it to the DB" do
+        expect {post :create, "link"=>{"url"=>"https://in.yahoo.com/?p=us"}}.to change(Link, :count).by(1)
+      end
+    end
 
+    context "given an invalid uri" do
+      it "doesn't save to DB" do
+        expect {post :create, "link"=>{"url"=>"google.com"}}.to change(Link, :count).by(0)
+      end
+    end
+  end
 end
