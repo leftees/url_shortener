836f2f1da84294adf1abe4c20762c18c39170f3e
Mohnish G J <mail4mohnish@gmail.com>
2015-03-24 06:00:26 +0530

    specs for checking valid URI

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#ensure_valid_uri" do
+    context "given a valid uri" do
+      it "should create a new link record" do
+        FactoryGirl.create(:link, :url => "https://in.yahoo.com/?p=us")
+        expect(Link.count).to eq(1)
+      end
+    end
+
+    context "given an invalid uri" do
+      it "should not create a new link record and raise an appropriate exception" do
+        expect {FactoryGirl.create(:link, :url => "google.com")}.to raise_error(ActiveRecord::RecordNotSaved)
+      end
+    end
+  end
+
 end
